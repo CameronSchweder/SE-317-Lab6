4b5a6e1f59df6ceebef335d997f7f304916551f7
nvoung <nathanvoung@gmail.com>
2025-04-14 16:57:55 -0500

    UtilityAccount changes

--- src/lab6/UtilityCompany.java
+++ src/lab6/UtilityCompany.java
@@ -1,5 +1,66 @@
 package lab6;
 
-public class UtilityCompany {
+import java.time.LocalDate;
+import java.util.*;
 
+public class UtilityCompany {
+	private int newAccountNum = 100000;
+	private String username;
+	private String password;
+	private int accountNum;
+	private double nextBillCost;
+	private LocalDate dueDate;
+	private ArrayList<String> billHistory;
+	
+	//initialization
+	public void createAccount(String user, String pass)
+	{
+		accountNum = newAccountNum;
+		username = user;
+		password = pass;
+        this.billHistory = new ArrayList<>();
+		newAccountNum++;
+	}
+	
+	private void createBill()
+	{
+		nextBillCost = Math.random() * 100;
+		dueDate = LocalDate.now();
+	}
+	
+	//logic
+	public boolean userLogin(String user, String pass)
+	{
+		return username.equals(user) && password.equals(pass);
+	}
+	
+	public void addBillPayment(int amount)
+	{
+		 if (billHistory.size() >= 3) {
+	            billHistory.remove(0); 
+	        }
+	        billHistory.add("Paid $" + amount + " on 2025-04-14");
+	        createBill();
+	    }
+
+	
+	
+	//Getters 
+	public int getAccountNum()
+	{
+		return accountNum;
+	}
+	public double nextBillCost()
+	{
+		return this.nextBillCost;
+	}
+	
+	public LocalDate getDueDate()
+	{
+		return this.dueDate;
+	}
+	
+	
+	
+	
 }
